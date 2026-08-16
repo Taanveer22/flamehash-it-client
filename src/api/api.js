6b9bfa0c69740a978746c3dcde9c2f0ffe5f45f8
Taanveer22 <taanveer469@gmailcom>
2026-08-17 01:49:07 +0600

    alhamdulliah local and production both live link added

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL:
+    import.meta.env.VITE_API_URL_LOCAL ||
+    import.meta.env.VITE_API_URL_PRODUCTION,
   withCredentials: true,
 });
 
